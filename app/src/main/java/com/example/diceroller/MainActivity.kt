8fd4ee1f9c3f7f92ba80e307191a4b63ed52939e
rahulsain3000 <50570677+rahulsain@users.noreply.github.com>
2020-12-13 12:12:06 +0530

    added 2 dice instead of 1

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,10 +1,9 @@
 package com.example.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 /**
  * This activity allows the user to roll a dice and view the result
@@ -16,17 +15,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.button)
-        rollButton.setOnClickListener { rollDice() }
+        rollButton.setOnClickListener {
+            rollFirstDice()
+            rollSecondDice()
+        }
     }
+
     /**
      * Roll the dice and update the screen with the result.
      */
-    private fun rollDice() {
+    private fun rollFirstDice() {
         // Create new Dice object with 6 sides and roll it
         val dice = Dice(6)
         val diceRoll = dice.roll()
         // Update the screen with the dice roll
-        val resultTextView: TextView = findViewById(R.id.textView)
+        val resultTextView: TextView = findViewById(R.id.dice1)
+        resultTextView.text = diceRoll.toString()
+    }
+
+    private fun rollSecondDice() {
+        // Create new Dice object with 6 sides and roll it
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        // Update the screen with the dice roll
+        val resultTextView: TextView = findViewById(R.id.dice2)
         resultTextView.text = diceRoll.toString()
     }
 
